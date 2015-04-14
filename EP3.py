5f93dffbe55dd3cb91148b0fda22c1880a1a7e62
MatheusDMD <matheusdmd@al.insper.edu.br>
2015-04-14 15:57:23 -0300

    2
    Gabi e Matheus

--- EP3.py
+++ EP3.py
@@ -5,9 +5,39 @@ Created on Tue Apr 14 14:43:11 2015
 Gabriela Almeida
 Matheus Dias M.
 """
+def HBH(p,h,i,f):
+    HB = 88.36+(13.4*p)+(4.8*h)-(5.7*i)
+    if f == 'grau mínimo' or f == 'mínimo' or f == 'minimo':
+        HB*=1.2
+    if f == 'baixo':
+        HB*=1.375
+    if f == 'média' or f == 'media':
+        HB*=1.55
+    if f == 'alto':
+        HB*=1.725
+    if f == 'muito alto':
+        HB*=1.9
+    return HB
+    
+def HBM(p,h,i,f):
+    HB = 447.6+(9.2*p)+(3.1*h)-(4.3*i)
+    if f == 'grau mínimo' or f == 'mínimo' or f == 'minimo':
+        HB*=1.2
+    if f == 'baixo':
+        HB*=1.375
+    if f == 'média' or f == 'media':
+        HB*=1.55
+    if f == 'alto':
+        HB*=1.725
+    if f == 'muito alto':
+        HB*=1.9
+    return HB
+    
 lista = []
+p = open('usuário.csv', encoding='latin1')
 b = open('alimentos.csv', encoding='latin1')
 al = b.readlines()
+
 
 for i in al:
     ali = i.strip().split(',')
@@ -19,3 +49,4 @@ l2 = [a[1:] for a in lista]
 
 alimentos = dict(zip(l1,l2))
 print(alimentos)
+
