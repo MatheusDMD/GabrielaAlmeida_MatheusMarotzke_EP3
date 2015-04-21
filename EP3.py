3d6db6ee081b192f944e9c963596eab945636b07
MatheusDMD <matheusdmd@al.insper.edu.br>
2015-04-21 18:42:18 -0300

    5

--- EP3.py
+++ EP3.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 14 14:43:11 2015
-
 Gabriela Almeida
 Matheus Dias M.
 """
@@ -34,19 +32,67 @@ def HBM(p,h,i,f):
     return HB
     
 lista = []
-c = open('usuario.csv')
+lis = []
+r = []
+e = []
+data_comida_cal = {}
+tdcal = []
+qtd = [0]
+
+
 b = open('alimentos.csv', encoding='latin1')
 al = b.readlines()
-us = c.readlines()
-print(us)
 for i in al:
     ali = i.strip().split(',')
     lista.append(ali)
 lista.remove(lista[0])
-
 l1 = [a[0] for a in lista]
 l2 = [a[1:] for a in lista]
-
 alimentos = dict(zip(l1,l2))
 
+c = open('usuario.csv')
+us = c.readlines()
+for i in us:
+    lis = i.strip().split(';')
+    r.append(lis)
+r.remove(r[0])
+r.remove(r[1])
 
+nome = r[0][0]
+idade = float(r[0][1])
+peso = float(r[0][2])
+sexo = r[0][3]
+altura = float(r[0][4])
+fator = r[0][5]
+
+x = HBM(peso,altura,idade,fator)
+
+for i in range(1,len(r)):
+    if r[i][0] not in data_comida_cal:
+        data_comida_cal[r[i][0]] = [[r[i][1],r[i][2]]]
+    else:
+        data_comida_cal[r[i][0]].append([r[i][1],r[i][2]])
+print(data_comida_cal)
+
+for i in data_comida_cal:
+    total=0
+    for j in data_comida_cal[i]:
+        total+=(float(alimentos[j[0]][1])/float(alimentos[j[0]][0]))*float(j[1])
+        #repond to dic
+'''
+for i in data_comida_cal:
+    qtd.append(len(data_comida_cal[i]))
+    for j in data_comida_cal[i]:
+        tdcal.append((float(alimentos[j[0]][1])/float(alimentos[j[0]][0]))*float(j[1]))
+k = []
+soma=0
+print(qtd)
+print(tdcal)
+print(data_comida_cal)
+for i in range(len(qtd)-1):
+    for j in range(qtd[i],qtd[i]+qtd[i+1]):
+        soma=soma+tdcal[j]
+    k.append(soma)
+    soma=0
+print(k)
+'''
